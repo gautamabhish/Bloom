42355ffbbeae1fa4fa5aa7e03cd14f85a9698dc8
HimanshuMahajan123 <147161044+HimanshuMahajan123@users.noreply.github.com>
2026-02-07 11:23:09 +0530

    refactor: reorganize imports and format code in auth.routes.js

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,9 +1,13 @@
-import { loginUser, verifyEmail } from "../controllers/auth.controllers.js";
+import {
+  loginUser,
+  verifyEmail,
+  getMe,
+} from "../controllers/auth.controllers.js";
 import { Router } from "express";
 
 const router = Router();
 
 router.post("/login", loginUser);
 router.get("/verify-email/:token", verifyEmail);
-router.get("/me",getMe);
+router.get("/me", getMe);
 export default router;
